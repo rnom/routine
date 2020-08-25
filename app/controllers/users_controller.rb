588ffef7ef7fd4e0d2d4527a1a14ec9853121d17
rnom <rnomura8@gmail.com>
2020-08-25 10:05:46 +0900

    add target for each graph

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,41 +25,53 @@ class UsersController < ApplicationController
     @n_day = Date.today.beginning_of_week(:monday)
 
     @chart_cal = []
+    @cal_target = []
     7.times do
       @meals = Meal.where(created_at: @n_day.all_day, user_id: current_user.id)
       @sum_cal = @meals.sum(:cal).round(0)
       array = [@n_day, @sum_cal]
       @chart_cal << array
+      array = [@n_day, 1800]
+      @cal_target << array
       @n_day += 1
     end
     
     @chart_protein = []
+    @protein_target = []
     @n_day = Date.today.beginning_of_week(:monday)
     7.times do
       @meals = Meal.where(created_at: @n_day.all_day, user_id: current_user.id)
       @sum_protein = @meals.sum(:protein).round(0)
       array = [@n_day, @sum_protein]
       @chart_protein << array
+      array = [@n_day, 150]
+      @protein_target << array
       @n_day += 1
     end
 
     @chart_fat = []
+    @fat_target = []
     @n_day = Date.today.beginning_of_week(:monday)
     7.times do
       @meals = Meal.where(created_at: @n_day.all_day, user_id: current_user.id)
       @sum_fat = @meals.sum(:fat).round(0)
       array = [@n_day, @sum_fat]
       @chart_fat << array
+      array = [@n_day, 60]
+      @fat_target << array
       @n_day += 1
     end
 
     @chart_carb = []
+    @carb_target = []
     @n_day = Date.today.beginning_of_week(:monday)
     7.times do
       @meals = Meal.where(created_at: @n_day.all_day, user_id: current_user.id)
       @sum_carb = @meals.sum(:carb).round(0)
       array = [@n_day, @sum_carb]
       @chart_carb << array
+      array = [@n_day, 90]
+      @carb_target << array
       @n_day += 1
     end
 
