825c16e289ebb127c1a1616ce73dc2d80158cb01
rnom <rnomura8@gmail.com>
2020-10-11 21:04:23 +0900

    add help_box for mypage

---
+++ app/assets/javascripts/help.js
@@ -0,0 +1,15 @@
+$(function() {
+  $('.personal_data_figures_bmr').click(function() {
+    $('.bmr_help_text.is-show').toggle();
+  })
+});
+$(function() {
+  $('.personal_data_figures_tdee').click(function() {
+    $('.tdee_help_text.is-show').toggle();
+  })
+});
+$(function() {
+  $('.personal_data_figures_pal').click(function() {
+    $('.pal_help_text.is-show').toggle();
+  })
+});
