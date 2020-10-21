a16e13d78cf94585cd3a40e195e18a17e20f0aa8
rnom <rnomura8@gmail.com>
2020-10-21 21:41:19 +0900

    add help-info

--- app/assets/javascripts/help.js
+++ app/assets/javascripts/help.js
@@ -1,36 +1,65 @@
 $(function() {
-  $('.personal_data_figures_bmr').click(function() {
+  $('.personal_data_figures_bmr').hover(function() {
     $('.bmr_help_text.is-show').toggle();
+    $(this).css("color", "blue");
+  }, function() {
+    $('.bmr_help_text.is-show').toggle();
+    $(this).css("color", "orange");
   })
 });
 $(function() {
-  $('.personal_data_figures_tdee').click(function() {
+  $('.personal_data_figures_tdee').hover(function() {
     $('.tdee_help_text.is-show').toggle();
+    $(this).css("color", "blue");
+  }, function() {
+    $('.tdee_help_text.is-show').toggle();
+    $(this).css("color", "orange");
   })
 });
 $(function() {
-  $('.personal_data_figures_pal').click(function() {
+  $('.personal_data_figures_pal').hover(function() {
     $('.pal_help_text.is-show').toggle();
-  })
-});
-$(function() {
-  $('.signup_account_data_area_title_pal').click(function() {
+    $(this).css("color", "blue");
+  }, function() {
     $('.pal_help_text.is-show').toggle();
+    $(this).css("color", "orange");
   })
 });
 
 $(function() {
-  $('.PFC_target_data_protein').click(function() {
+  $('.signup_account_data_area_title_pal').hover(function() {
+    $('.pal_help_text.is-show').toggle();
+    $(this).css("color", "blue");
+  }, function() {
+    $('.pal_help_text.is-show').toggle();
+    $(this).css("color", "orange");
+  })
+});
+
+$(function() {
+  $('.PFC_target_data_protein').hover(function() {
     $('.protein_help_text.is-show').toggle();
+    $(this).css("color", "blue");
+  }, function() {
+    $('.protein_help_text.is-show').toggle();
+    $(this).css("color", "orange");
   })
 });
 $(function() {
-  $('.PFC_target_data_fat').click(function() {
+  $('.PFC_target_data_fat').hover(function() {
     $('.fat_help_text.is-show').toggle();
+    $(this).css("color", "blue");
+  }, function() {
+    $('.fat_help_text.is-show').toggle();
+    $(this).css("color", "orange");
   })
 });
 $(function() {
-  $('.PFC_target_data_carb').click(function() {
+  $('.PFC_target_data_carb').hover(function() {
     $('.carb_help_text.is-show').toggle();
+    $(this).css("color", "blue");
+  }, function() {
+    $('.carb_help_text.is-show').toggle();
+    $(this).css("color", "orange");
   })
 });
